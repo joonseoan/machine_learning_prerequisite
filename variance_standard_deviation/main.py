51e7f052c6e8093beeddc95b4eb4d97ec581493f
joon <joonseo.an@outlook.com>
2024-02-01 01:50:36 -0500

    concept of pmf and pdf

--- variance_standard_deviation/main.py
+++ variance_standard_deviation/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Tomorrow. Let's look at the standard deviation and variance concept more in the internet
 
 # 100.0 : center number
-# 20.0 : standard deviation
+# 50.0 : standard deviation
 # 10000: data points
 incomes = np.random.normal(100.0, 50.0, 10000)
 print(incomes.std())  # standard deviation (square root of variance)
